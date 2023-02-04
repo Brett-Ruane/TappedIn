5ffd03f25544e72a46c67c4058d1b5722cad1032
Hoang Nguyen <90430178+hoangnguyen2021@users.noreply.github.com>
2023-02-04 17:18:21 -0500

    Homepage

--- UGAHacks-Tapped-In/App.js
+++ UGAHacks-Tapped-In/App.js
@@ -1,3 +1,5 @@
+import { ThemeProvider, createTheme } from "@rneui/themed";
+import { useFonts } from "expo-font";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -8,16 +10,35 @@ import ProfileScreen from "./screens/ProfileScreen";
 const Tab = createBottomTabNavigator();
 
 const App = () => {
+  const [fonts] = useFonts({
+    "Dosis-Regular": require("./assets/fonts/Dosis-Regular.ttf"),
+    "Dosis-Medium": require("./assets/fonts/Dosis-Medium.ttf"),
+    "Dosis-SemiBold": require("./assets/fonts/Dosis-SemiBold.ttf"),
+    "Dosis-Bold": require("./assets/fonts/Dosis-Bold.ttf"),
+  });
+
+  const theme = createTheme({
+    lightColors: {
+      primary: "#3FB0BF",
+    },
+    darkColors: {
+      primary: "#000",
+    },
+    mode: "light",
+  });
+
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Create Event" component={CreateEventScreen} />
-          <Tab.Screen name="Profile" component={ProfileScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <ThemeProvider theme={theme}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Create Event" component={CreateEventScreen} />
+            <Tab.Screen name="Profile" component={ProfileScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </ThemeProvider>
   );
 };
 
