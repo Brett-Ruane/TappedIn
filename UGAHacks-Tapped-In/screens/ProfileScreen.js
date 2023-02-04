ef3e5a9fe961142361322750e504af29c6b5ee4e
ky06134 <ky06134@uga.edu>
2023-02-04 14:46:32 -0500

    profileScreen
    format completed

--- UGAHacks-Tapped-In/screens/ProfileScreen.js
+++ UGAHacks-Tapped-In/screens/ProfileScreen.js
@@ -1,30 +1,84 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import { Card } from "@rneui/themed";
 
-const App = () => (
-  <View style={styles.container}>
-    <Text style={styles.title}>React Native</Text>
-  </View>
-);
+//import all the components we are going to use
+import {
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  ScrollView
+} from 'react-native';
+
+const App = () => {
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <Image
+          source={{
+            uri:
+              'https://raw.githubusercontent.com/AboutReact/sampleresource/master/old_logo.png',
+          }}
+          //borderRadius will help to make Round Shape
+          style={{
+            top: 30,
+            width: 200,
+            height: 200,
+            overflow: "hidden",
+            borderWidth: 2,
+            borderColor: "black",
+            borderRadius: 200 / 2
+          }}
+        />
+        <Text style={styles.textHeadingStyle}>
+          Username
+        </Text>
+        <Text style={styles.baseText}>
+          TAGS
+        </Text>
+        <View style={styles.textContainer}>
+          <Text style={styles.description}>
+          TO BE FILLED
+        </Text>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    padding: 24,
-    backgroundColor: '#eaeaea',
+    width: '100%',
+    height: 900,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
   },
-  title: {
-    marginTop: 16,
-    paddingVertical: 8,
-    borderWidth: 4,
-    borderColor: '#20232a',
-    borderRadius: 6,
-    backgroundColor: '#61dafb',
-    color: '#20232a',
-    textAlign: 'center',
-    fontSize: 30,
+  textHeadingStyle: {
+    marginTop: 30,
+    fontSize: 40,
+    color: 'black',
     fontWeight: 'bold',
   },
+  baseText: {
+    fontSize: 20,
+    color: 'black',
+  },
+  textContainer: {
+    width: 400,
+    height: 100,
+    top: 20,
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'left',
+    backgroundColor: 'white',
+    borderColor: 'black'
+
+  },
+  description: {
+    fontSize: 20,
+
+  }
 });
 
 export default App;
