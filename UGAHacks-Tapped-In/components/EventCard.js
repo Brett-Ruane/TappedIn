9e9d370e9e8230d34d3975732472346f0142f070
Hoang Nguyen <90430178+hoangnguyen2021@users.noreply.github.com>
2023-02-04 20:41:51 -0500

    123

--- UGAHacks-Tapped-In/components/EventCard.js
+++ UGAHacks-Tapped-In/components/EventCard.js
@@ -1,7 +1,34 @@
 import { View, StyleSheet, TouchableHighlight } from "react-native";
 import { Card, Text, Image, useTheme } from "@rneui/themed";
 
-const EventCard = () => {
+const DATA = [
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
+  },
+  {
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
+  },
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28bf",
+    title: "First Item",
+  },
+  {
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f6r",
+    title: "Second Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d7t",
+    title: "Third Item",
+  },
+];
+
+const EventCard = ({ onClick }) => {
   const { theme } = useTheme();
 
   const styles = StyleSheet.create({
